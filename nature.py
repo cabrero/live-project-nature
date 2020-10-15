76ef4c4b0f1b119c4ee20e02ed2d3eee286e96af
David Cabrero <david.cabrero@madsgroup.org>
2020-10-15 18:13:26 +0200

    2.1. Adding drops

--- nature.py
+++ nature.py
@@ -10,6 +10,22 @@ PADDING = 10
 NROWS = 20
 NCOLS = 20
 
+COL_STEP = WIDTH // NCOLS
+ROW_STEP = HEIGHT // NROWS
+
+RAIN_RATE = 0.1
+INIT_SIZE = 2
+DROP_INCREASE = 5
+RESET_SIZE = 50
+
+def update(canvas, drops):
+    for i, col in enumerate(drops):
+        for j, sz in enumerate(col):
+            x = i * COL_STEP
+            y = j * ROW_STEP
+            r = sz // 2
+            canvas.create_oval(x - r, y - r, x + r, y + r)
+            
 def main():
     window = tk.Tk()
     window.title("Live Project: Simulating Nature")
@@ -19,10 +35,15 @@ def main():
     canvas = tk.Canvas(master= window, width= WIDTH, height= HEIGHT)
     canvas.pack(anchor= tk.CENTER, expand= False)
 
-    for col in range(0, WIDTH, WIDTH // NCOLS):
-        for row in range(0, HEIGHT, HEIGHT // NROWS):
+    for col in range(0, WIDTH, COL_STEP):
+        for row in range(0, HEIGHT, ROW_STEP):
             canvas.create_line(0, row, WIDTH, row)
             canvas.create_line(col, 0, col, HEIGHT)
+
+    drops = [ [ INIT_SIZE for _ in range(0, NROWS)] for _ in range(0, NCOLS) ]
+
+    update(canvas, drops)
+    
     window.mainloop()
 
     
